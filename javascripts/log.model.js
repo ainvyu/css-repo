bd175a8d6b06dd9020965d967bf98720c50a2bbd
Nipol <Nipol@cosmo.so>
2012-10-03 02:17:34 +0900

    [JS] 전반적 변경

--- javascripts/log.model.js
+++ javascripts/log.model.js
@@ -1,65 +1,63 @@
 var isChatting = false;
 
 function _fill_zero(dec, len) {
-  var obj = '000000000000000'+dec;
-  return obj.substring(obj.length - len);
+	var obj = '000000000000000'+dec;
+	return obj.substring(obj.length - len);
 }
 
 function _make_line(msg) {
   var date = new Date(msg.time*1000);
   var dateForm =
-    _fill_zero(date.getHours(), 2)
-      + ":" + _fill_zero(date.getMinutes(), 2)
-      + ":" + _fill_zero(date.getSeconds(), 2);
+		_fill_zero(date.getHours(), 2)
+		+ ":" + _fill_zero(date.getMinutes(), 2)
+		+ ":" + _fill_zero(date.getSeconds(), 2);
 
-	var data = 	'<li id=line' + msg.no + '>';
-		data += '  <div class="log_line">';
-		data +=	'    <span class="log_time" title=' + dateForm + '>' + '<a href="?scroll=line' + msg.no + '">' + dateForm + '</a>' + '</span>';
-		data +=	'    <span class="log_nickname">'+ msg.nick + '</span>';
-		data +=	'    <span class="log_message">' + _filter_data(msg.what) + '</span>';
-		data += '  </div>';
-		data +=	'</li>';
+	var data =  '<li id=line' + msg.no + '>';
+		data += '	<div class="log_line">';
+		data += '		<span class="log_time" title=' + dateForm + '>' + '<a href="?scroll=line' + msg.no + '">' + dateForm + '</a>' + '</span>';
+		data += '		<span class="log_nickname">'+ msg.nick + '</span>';
+		data += '		<span class="log_message">' + _filter_data(msg.what) + '</span>';
+		data += '	</div>';
+		data += '</li>';
 
-  return data;
+	return data;
 }
 
 function _append_log(force_scroll, msg) {
-  var _from = from,
-    $doc = $(document),
-    $window = $(window),
-    appendTarget = $('.page > .scroller');
+	var _from = from,
+		$doc = $(document),
+		$window = $(window),
+		appendTarget = $('.scroller');
 
-  console.log("window.scrollTop: " + $(window).scrollTop());
-  console.log("document.height(): " + $(document).height());
-  console.log("window.height: " + $(window).height());
-  console.log("document.height()-window.height(): " + ($(document).height() - $(window).height()));
+	console.log("window.scrollTop: " + $(window).scrollTop());
+	console.log("document.height(): " + $(document).height());
+	console.log("window.height: " + $(window).height());
+	console.log("document.height()-window.height(): " + ($(document).height() - $(window).height()));
 
-  if ($(window).scrollTop() >= ($(document).height() - $(window).height())) {
-    console.log("now scroll has bottom.")
-  }
+	if ($(window).scrollTop() >= ($(document).height() - $(window).height())) {
+		console.log("now scroll has bottom.")
+	}
 
-  var willScroll = force_scroll || isScrollBottom || isChatting;
+	var willScroll = force_scroll || isScrollBottom || isChatting;
 
-  appendTarget.append(_make_line(msg));
+	appendTarget.append(_make_line(msg));
+	unloader.reset();
+	myScroll.refresh();
 
-  unloader.reset();
-  myScroll.refresh();
-
-  if (willScroll) {
-    scroll_to_bottom();
-  }
+	if (willScroll) {
+		scroll_to_bottom();
+	}
 }
 
 function _transform_url(data) {
-  var re = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/g;
-
-  return data.replace(re, '<a href="' + '$1' + '" target=_blank>$1</a>');
+	var re = /\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?]))/g;
+	return data.replace(re, '<a href="' + '$1' + '" target=_blank>$1</a>');
 }
 
 function _filter_data(data) {
-  var filtered = data;
-  filtered = _transform_url(filtered);
-  return filtered;
+	var filtered = data;
+	filtered = _transform_url(filtered);
+	return filtered;
 }
 
 function _get_log(log_req_addr, limit, offset) {
@@ -69,7 +67,7 @@ function _get_log(log_req_addr, limit, offset) {
 	var _from = from,
 		$doc = $(document),
 		$window = $(window),
-		appendTarget = $('.page > .scroller');
+		appendTarget = $('.scroller');
 	var init_data = '';
 
 	$.getJSON(log_fetch_addr, function(msgs) {
@@ -78,6 +76,7 @@ function _get_log(log_req_addr, limit, offset) {
 		});
 	
 		appendTarget.append(init_data);
+		unloader.reset();
         myScroll.refresh();
 	
 		if (scroll_line != '') {
@@ -87,40 +86,25 @@ function _get_log(log_req_addr, limit, offset) {
 }
     
 function _init_log() {
-<<<<<<< HEAD
-    var wrapper = document.querySelector('#content');
-=======
 	var wrapper = d.querySelector('#log_view .content'),
->>>>>>> [JS] 언로더 에러 픽스
 	unloader = new Unloader(wrapper, 'li');
 	
 	myScroll = new iScroll(wrapper, {
         onRefresh: function () {
         	unloader.setup(this.x, this.y);
-        },
-        onScrollEnd: function () {
             // Tracking scroll for detect scroll bottom
-<<<<<<< HEAD
-            if (myScroll.y >= $('#content').height) {
-                //console.log("Scroll bottom");
-                isScrollBottom = true;
-            }
-            else {
-                //console.log("Scroll non bottom:" + myScroll.y + ' ' + $('#content').children[0].offsetHeight);
-=======
             if (myScroll.y >= $('.content').height) {
                 console.log("Scroll bottom");
                 isScrollBottom = true;
             }
             else {
                 console.log("Scroll non bottom:" + myScroll.y + ' ' + $('.content').children[0].offsetHeight);
->>>>>>> [JS] 언로더 에러 픽스
                 isScrollBottom = false;
             }
         },
         onPositionChange: function(x, y) {
             unloader.onmove(x, y);
-        },
+        }
     });
 	_get_log(log_data_addr, 999999999, last_log_no+1);
 }
@@ -157,14 +141,12 @@ function start_chat() {
 }
 
 $('#msg').focus(function(event) {
-    console.log('chatting flag on');
     isChatting = true;
     unloader.reset();
     myScroll.refresh();
 });
 
 $('#msg').focusout(function(event) {
-    console.log('chatting flag off');
     isChatting = false;
     unloader.reset();
     myScroll.refresh();
@@ -197,13 +179,10 @@ function submit_say(msg) {
     return;
   }
 
-  var msg_data = { 
-      nick: nickname,
-      what: msg,
-      time: Math.floor(new Date().getTime() / 1000),
-      channel: from,
-      type: 0 
-  };
+  var msg_data = { what: msg,
+                   time: Math.floor(new Date().getTime() / 1000),
+                   channel: from,
+                   type: 0 };
 
   socket.emit('chat.privmsg', msg_data);
 }
